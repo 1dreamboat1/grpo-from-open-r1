4fd9cba93a380959e147b4ddebe6a2ad74a795a4
Mryangkaitong <1500936094@qq.com>
2025-02-10 16:35:42 +0800

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,6 +1,6 @@
 accelerate launch --config_file recipes/accelerate_configs/zero3.yaml --num_processes=3 src/open_r1/grpo.py \
     --output_dir DeepSeek-R1-Distill-Qwen-15B-GRPO \
-    --model_name_or_path /group/40092/20025/kaitongyang/model/Qwen/Qwen25-15B-Instruct \
+    --model_name_or_path Qwen25-15B-Instruct \
     --dataset_name data/gsm8k/main \
     --max_prompt_length 512 \
     --max_completion_length 1024 \
